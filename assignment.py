285bf4a01ee5799ed6c04fbabb59f0217325a928
Hemachowdary_N <hemachowdary.narasani@gmail.com>
2026-09-13 08:30:56 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,69 @@
+# personalized Greeting 
+name = input()
+print(F"Hello,{name}!")
+
+# Display personal details using variables
+# Greeting the input from user
+name = input()
+age = int(input())
+height = float(input())
+# printing the values
+print(name)
+print(age)
+print(height)
+
+# Add two numbers read as strings
+# Taken the input as a string
+a = input()
+b = input()
+# Converting the string into integer 
+a = int(a)
+b = int(b)
+# Find the sum
+total = a+b
+# print the result
+print(total)
+
+# Float to integer conversion
+n = float(input())
+print(n)
+new = int(n)
+print(new)
+
+# Sum using arithmetic operator
+a = int(input())
+b = int(input())
+print(a+b)
+
+# Area of a rectangle
+length = float(input())
+breadth = float(input())
+area = length*breadth
+print(area)
+
+# Quotient and remainder
+a = int(input())
+b = int(input())
+q = a/b
+r = a%b
+print(q)
+print(r)
+
+# power calculation
+base = int(input())
+exponent = int(input())
+print(base**exponent)
+
+# Average of three numbers
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+total = n1+n2+n3
+avg = total/3
+print(avg)
+
+# Greater than comparision
+a = int(input())
+b = int(input())
+print(a>b)
+
