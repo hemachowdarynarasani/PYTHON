aea78aff8e490dd30370eb0c34219f2cfbf09946
Hemachowdary_N <hemachowdary.narasani@gmail.com>
2026-09-13 08:57:32 +0530

    Add files via upload

---
+++ assignments.py
@@ -0,0 +1,26 @@
+# Equality check
+# check whether both the number are same or not
+n1 = int(input())
+n2 = int(input())
+print(n1 == n2)
+
+# Both numbers positive check
+n1 = int(input())
+n2 = int(input())
+print(n1>0 and n2>0)
+
+# atleast one even number 
+n1 = int(input())
+n2 = int(input())
+print(n1%2==0 or n2%2==0)
+
+# logical not on a condition
+num = int(input())
+print(not(num>0))
+
+#augmented assignment operations
+a = int(input()) #20
+a = a+5 #a = 20+5--> 25
+a = a*2 #a = 25*2--> 50
+a = a-3 #a = 50-3--> 47
+print(a)
